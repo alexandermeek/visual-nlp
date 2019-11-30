c77ff6248d4f7d772648b2c260b23a7758849e67
Alexander Meek <alexandermeek09@gmail.com>
2019-11-30 13:18:27 +0000

    Add partial implementation of node and node link class.

--- Node.h
+++ Node.h
@@ -1,5 +1,17 @@
+#ifndef NODE_H
+#define NODE_H
 #pragma once
+#include "imgui.h"
 class Node
 {
-};
+private:
+	int id;
+	ImVec2 pos, size;
+	int inputs_count, outputs_count;
+public:
+	Node(int id, ImVec2 pos, ImVec2 size, int inputsCount, int outputsCount);
 
+	ImVec2 GetInputSlotPos(int slotNum);
+	ImVec2 GetOutputSlotPos(int slotNum);
+};
+#endif //NODE_H
---
+++ node_link.h
@@ -0,0 +1,13 @@
+#ifndef NODELINK_H
+#define NODELINK_H
+#pragma once
+#include "imgui.h"
+class NodeLink
+{
+public:
+	int input_id;
+	int input_slot;
+	int output_id;
+	int output_slot;
+};
+#endif //NODELINE_H
