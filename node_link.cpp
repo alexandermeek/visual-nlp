fe38b18ca8129797bafab28a5cbaa258a9b2811a
Alexander Meek <alexandermeek09@gmail.com>
2019-12-02 18:15:12 +0000

    Add deletion options basic link creation support.

--- node_link.cpp
+++ node_link.cpp
@@ -2,3 +2,12 @@
 
 NodeLink::NodeLink(int input_id, int input_slot, int output_id, int output_slot)
 	: input_id(input_id), input_slot(input_slot), output_id(output_id), output_slot(output_slot) {}
+
+void NodeLink::SwapDirection() {
+	int temp_input_id = input_id;
+	int temp_input_slot = input_slot;
+	input_id = output_id;
+	input_slot = output_slot;
+	output_id = temp_input_id;
+	output_slot = temp_input_slot;
+}
--- node_link.h
+++ node_link.h
@@ -10,5 +10,6 @@ public:
 	int		output_slot;
 
 	NodeLink(int input_id, int input_slot, int output_id, int output_slot);
+	void SwapDirection();
 };
 #endif  // NODELINE_H
