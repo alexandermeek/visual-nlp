1ad28e9a5a61c3585f55fbc1206168a5010b3a68
Alexander Meek <alexandermeek09@gmail.com>
2020-02-18 10:15:15 +0000

    Reorganise link structure. Store in encapsulated vector in main function. Remove redundant conn inheritance.

--- node_link.cpp
+++ node_link.cpp
@@ -1,16 +1,6 @@
 #include "node_link.h"
 #include <iostream>
 
-NodeLink::NodeLink(OutputConn* start, InputConn* end) : start(start), end(end) {
-	start->AddLink(this);
-	end->AddLink(this);
-}
+NodeLink::NodeLink(NodeConn* start, NodeConn* end) : start(start), end(end) {}
 
-NodeLink::~NodeLink() {
-	if (start != nullptr) {
-		start->RemoveLink(this);
-	}
-	if (end != nullptr) {
-		end->RemoveLink(this);
-	}
-}
+NodeLink::~NodeLink() {}
